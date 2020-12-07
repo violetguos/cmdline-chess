e905c6805ba0dc09fa8ee13f8ccb549af21934d1
Y. Violet Guo <violetguos@users.noreply.github.com>
2020-12-07 15:41:22 -0400

    delete diag gettor

--- spec/coord_spec.rb
+++ spec/coord_spec.rb
@@ -4,28 +4,16 @@ describe Arr2D do
   it "finds an object in a 2d array" do
     b = Arr2D.new(8)
     loc = Coord2D.new(2, 1)
-    b[loc.x, loc.y] = "test"
-    expect(b[loc.x, loc.y]).to eql("test")    
+    b[loc] = "test"
+    expect(b[loc]).to eql("test")    
   end
 
   it "returns an error when coordinates are out of boundary" do
     b = Arr2D.new(4)
     loc = Coord2D.new(5, 6)
-    expect{b[loc.x, loc.y]}.to raise_error(RuntimeError, 'Coord out of boundary')
+    expect{b[loc]}.to raise_error(RuntimeError, 'Coord out of boundary')
   end
 
-  it "looks up the diags and return 2 arrays of Struct Coord2D" do
-    b = Arr2D.new(3)
-    b[0, 0] = 0
-    b[1, 1] = 1
-    b[2, 2] = 2
-
-    b[2, 0] = 2
-    b[1, 1] = 1
-    b[0, 2] = 0
-    # NOTE: it's not a list of 2 lists, only required for rspec syntax
-    expect(b.get_diag).to eql([[2, 1, 0], [0, 1, 2]])
-  end
 
   it "performs arithmetic" do 
     loc = Coord2D.new(2, 1)
